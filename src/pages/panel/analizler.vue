7b28d68e87bb53e85dd9f0c58af15966b521d653
Barış Kurt <45626175+baris-kurt@users.noreply.github.com>
2024-08-09 19:40:20 +0300

    init

---
+++ src/pages/panel/analizler.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <VCard
+        class="mb-6"
+        title="Motiwy - Tüm Analizler"
+      >
+        <VCardText>Tüm Analizler</VCardText>
+       
+      </VCard>
+      
+    </div>
+  </template>
+  
